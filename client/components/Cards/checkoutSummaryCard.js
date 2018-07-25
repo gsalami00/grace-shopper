b6c896d49d5b1df75b23a31968b4206bd602e1fc
Geraldine Salamat <g.a.salamat@gmail.com>
2018-07-25 18:30:20 -0400

    Cards Components

--- client/components/Cards/checkoutSummaryCard.js
+++ client/components/Cards/checkoutSummaryCard.js
@@ -1,38 +1,39 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class checkoutSummaryCard extends Component {
-  render() {
-    return (
-      <div class="ui card">
-        <div class="content">
-          <div class="header">Summary (insert # of items)</div>
-        </div>
-        <div class="content">
-          <table class="ui definition table">
-            <tbody>
-              <tr>
-                <td class="two wide column">Subtotal</td>
-                <td>insert subtotal</td>
-              </tr>
-              <tr>
-                <td>Shipping</td>
-                <td>FREE</td>
-              </tr>
-              <tr>
-                <td>Est. Taxes</td>
-                <td>insert taxes</td>
-              </tr>
-              <tr>
-                <td>TOTAL</td>
-                <td>insert total</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="extra content">
-          <button class="ui button">Join Project</button>
-        </div>
+const CheckoutSummaryCard = (props) => {
+  return (
+    <div className="ui card">
+      <div className="content">
+        <div className="header">Summary (insert # of items)</div>
       </div>
-    )
-  }
+      <div className="content">
+        <table className="ui definition table">
+          <tbody>
+            <tr>
+              <td className="two wide column">Subtotal</td>
+              <td>insert subtotal</td>
+            </tr>
+            <tr>
+              <td>Shipping</td>
+              <td>FREE</td>
+            </tr>
+            <tr>
+              <td>Est. Taxes</td>
+              <td>insert taxes</td>
+            </tr>
+            <tr>
+              <td>TOTAL</td>
+              <td>insert total</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="extra content">
+        <button className="ui button">Checkout</button>
+      </div>
+    </div>
+  )
 }
+
+
+export default CheckoutSummaryCard
--- client/components/Cards/itemCardCart.js
+++ client/components/Cards/itemCardCart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class itemCardCart extends Component {
+export default class ItemCardCart extends Component {
   render() {
     return (
       <div>
