7aabfea7144734181f9e6efc14ae8b39e2d9a1d8
Andrew Drummond <addrummond61@gmail.com>
2018-07-27 16:44:18 -0400

    added stripe test functionality with checkout component

---
+++ server/api/stripe.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { CartItem, User, Animal } = require('../db/models');
+const stripe = require("stripe")("sk_test_FglRX3tkNhaVmQUzXQFrj2ZA");
+module.exports = router
+
+router.post('/charge', async (req, res, next) => {
+  try {
+    const token = req.body.stripeToken;
+
+    let charge = await stripe.charges.create({
+      amount: 90000000,
+      currency: "usd",
+      description: "An example charge",
+      source: token
+    });
+    res.send(charge.status);
+  } catch (err) {
+    next(err)
+  }
+});
